104cbd4f2f08ab53574fd1d2c740eb5affbcc196
Artem <lolod.naugolnov@yandex.ru>
2023-12-05 19:13:56 +1000

    Мелкие добавки

--- AngularPraktik/ClientApp/src/app/data/menuCafe.ts
+++ AngularPraktik/ClientApp/src/app/data/menuCafe.ts
@@ -8,6 +8,14 @@ export const MenuCafe: IMenuCafe[] = [
     name: "Ponzu tofu",
     price: 10,
     vegan: true
+  },
+  {
+    category: { childrenMenu: false, section: 'meat' },
+    id: 2,
+    image: "https://images.immediate.co.uk/production/volatile/sites/30/2023/01/Ponzu-tofu-poke-bowl-8733c67.jpg?quality=90&resize=440,400",
+    name: "meat",
+    price: 10,
+    vegan: false
   }
 ]
 
